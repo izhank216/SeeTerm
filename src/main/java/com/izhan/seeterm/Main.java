07480a0c77ba299bf00d1e5e332f28e1b04e2152
izhank216 <izhank216@gmail.com>
2026-01-15 18:35:06 +0500

    Update Main.java

--- src/main/java/com/izhan/seeterm/Main.java
+++ src/main/java/com/izhan/seeterm/Main.java
@@ -25,6 +25,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.verbose", "true");
+        System.setProperty("javafx.macosx.embedded", "true");
+
         SwingUtilities.invokeLater(() -> {
 
             try {
