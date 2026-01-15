e64cdf827f4f02ca40dc736fd0f5bb1ee37c7c6d
izhank216 <izhank216@gmail.com>
2026-01-15 17:28:45 +0500

    Create SSHInfo.java

---
+++ src/main/java/com/izhan/seeterm/SSHInfo.java
@@ -0,0 +1,30 @@
+package com.izhan.seeterm;
+
+public class SSHInfo {
+
+    private String server;
+    private int port;
+
+    public SSHInfo() {}
+
+    public SSHInfo(String server, int port) {
+        this.server = server;
+        this.port = port;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
